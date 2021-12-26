e0791d5161a6f725d421f09b254a7d79f571baac
shiyalei <shiyalei@baimaohui.net>
2021-12-26 14:45:56 +0800

    zorm adds some example

--- orm/zorm.go
+++ orm/zorm.go
@@ -6,6 +6,8 @@ import (
 	"gitee.com/chunanyong/zorm"
 )
 
+// Where for sql where
+// example: Where(finder, "col1=? and col2=?", 1, "3") --> where col1=? and col2=? args:[1, "3"]
 func Where(finder *zorm.Finder, cond string, values ...interface{}) *zorm.Finder {
 	query, _ := finder.GetSQL()
 	query = strings.ToLower(query)
@@ -18,6 +20,9 @@ func Where(finder *zorm.Finder, cond string, values ...interface{}) *zorm.Finder
 	return finder
 }
 
+// Or adds sql or condition
+// example: Or(finder, "id>?", 1) --> or id>? args:[1]
+// example: Or(finder, "id=? and name != ?", 10, "John") --> or (id=? and name!=?) args:[10, "John"]
 func Or(finder *zorm.Finder, cond string, values ...interface{}) *zorm.Finder {
 	if strings.Trim(cond, " ") == "" {
 		return finder
@@ -40,7 +45,7 @@ func Order(finder *zorm.Finder, cols ...string) *zorm.Finder {
 	}
 
 	var builder strings.Builder
-	finder.Append("order by")
+	builder.WriteString("order by ")
 	builder.WriteString(cols[0])
 	if len(cols) > 1 {
 		length := len(cols)
@@ -48,5 +53,6 @@ func Order(finder *zorm.Finder, cols ...string) *zorm.Finder {
 			builder.WriteString("," + cols[i])
 		}
 	}
+
 	return finder.Append(builder.String())
 }
